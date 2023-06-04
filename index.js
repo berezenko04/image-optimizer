951b5d354fa1fc93c9f45d511f42ba6eb644f31f
berezenko04 <koffld@yahoo.com>
2023-06-04 04:14:41 +0300

    release

---
+++ index.js
@@ -0,0 +1,41 @@
+import sharp from "sharp";
+import imagemin from "imagemin";
+import imageminWebp from "imagemin-webp";
+import fs from "fs";
+import path from "path";
+
+const inputFolderPath = "input/";
+
+const outputFolderPath = "output/";
+
+if (!fs.existsSync(outputFolderPath)) {
+  fs.mkdirSync(outputFolderPath);
+}
+
+async function optimizeImages() {
+  try {
+    const files = fs.readdirSync(inputFolderPath);
+
+    for (const file of files) {
+      const inputFilePath = path.join(inputFolderPath, file);
+      const outputFileName = `${path.parse(file).name}.webp`;
+      const outputFilePath = path.join(outputFolderPath, outputFileName);
+
+      const resizedImage = await sharp(inputFilePath)
+        .resize(1000, 1000, { fit: "inside" })
+        .toBuffer();
+
+      const optimizedImageBuffer = await imagemin.buffer(resizedImage, {
+        plugins: [imageminWebp({ quality: 100 })],
+      });
+
+      fs.writeFileSync(outputFilePath, optimizedImageBuffer);
+    }
+
+    console.log("Images optimized successfully.");
+  } catch (error) {
+    console.error("Error optimizing images:", error);
+  }
+}
+
+optimizeImages();
